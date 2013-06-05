d582ff71d3834b5d3cdd5095a42a612dde1371c1
Jeff Casimir <jeff@casimircreative.com>
2013-06-05 11:46:42 -0600

    Beginning scrabble-web project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ScrabbleWeb::Application.config.secret_token = 'd46c35e8791e63d5204e7051f39076ebd85401498c76487396d856c67a4b7c54d94674ab8d11e230e2a1fcd555ab5cf6eb960c31f4282b76b573dd28bdd77d64'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+ScrabbleWeb::Application.config.session_store :cookie_store, key: '_scrabble-web_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# ScrabbleWeb::Application.config.session_store :active_record_store
