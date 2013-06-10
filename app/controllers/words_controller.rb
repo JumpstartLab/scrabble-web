b45accc8ea81a3e2ed08bfc4d8e636226b1421d9
Jeff Casimir <jeff@casimircreative.com>
2013-06-10 15:56:03 -0600

    Stripped down to student-ready

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,17 +1,7 @@
 class WordsController < ApplicationController
   def index
-    @last_word = Word.find(params[:last_word_id]) if params[:last_word_id]
-    @previous_words = Word.all
-    @new_word = Word.new
   end
 
   def create
-    @word = Word.new(params[:word])
-    if @word.save
-      redirect_to words_path(:last_word_id => @word.id)
-    else
-      flash[:notice] = "Sorry, please enter a single word made up of only letters"
-      redirect_to words_path
-    end
   end
 end
--- spec/models/word_spec.rb
+++ spec/models/word_spec.rb
@@ -2,25 +2,18 @@ require 'spec_helper'
 
 describe Word do
   describe 'validations' do
-    it "is valid with a word" do
+    xit "is valid with a word" do
       expect(Word.new(:word => "hi")).to be_valid
     end
 
-    it "is not valid without a word" do
+    xit "is not valid without a word" do
       expect(Word.new).to_not be_valid
     end
 
-    it "does not allow non-letters in words" do
-      ['two words', 'exclaim!', '37numbers'].each do |text|
-        expect(Word.new(:word => text)).to_not be_valid
-      end
-    end
+    it "does not allow non-letters in words"
   end
 
   describe '.compute_score' do
-    it "scores hello as 8" do
-      w = Word.new(:word => 'hello')
-      expect(w.compute_score).to eq 8
-    end
+    it "scores hello as 8"
   end
 end
