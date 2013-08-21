b8c39018c4448fe21a34fe1e03e0904711885775
Justin Leveck <jleveck@gmail.com>
2013-08-21 19:43:09 +0000

    Set previous words specs as pending

--- spec/features/previous_words_spec.rb
+++ spec/features/previous_words_spec.rb
@@ -13,7 +13,7 @@ describe "previous words" do
         end
       end
 
-      it "displays the last three words along with their scores" do
+      xit "displays the last three words along with their scores" do
         within("#previous_words") do
           sample_words.last(3).each do |word, score|
             within("#word-#{word.downcase}") do
